43535e1d73ad5a4bacae8acd5ab72dc5c6e9ac97
Tarık Çekirge <tcekirge@gmail.com>
2025-06-08 20:54:21 +0300

    Protecting Routes With NextAuth Middleware

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 
 export default async function Navigation() {
   const session = await auth();
-  console.log(session);
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex gap-16 items-center'>
